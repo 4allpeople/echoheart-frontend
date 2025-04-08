280fefc2226205e0084f577796597ebf57c93337
4allpeople <myfifthhouse5@gmail.com>
2025-04-08 09:52:15 -0400

    add tailwind.config postcss.config

---
+++ init-tailwind.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+node ./node_modules/tailwindcss/lib/cli.js init -p
+
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- ensures relative paths for static assets (required for Render)
+  root: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
-
